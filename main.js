36247296fafbe8b61e88cab8f246353bd5306f5e
Vinicius Santos <vinisantoosk@gmail.com>
2023-05-12 14:26:31 -0300

    branch initial commit

--- main.js
+++ main.js
@@ -1,52 +1,19 @@
-const form = document.getElementById('form');
-let formEstaValido = false;
-
-function validaComparacao(campoUm,campoDois) {
-   return campoDois>campoUm;
-}
-
-
-
-
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    
-    const campoA = document.getElementById('campo-a');
-    console.log(campoA.value)
-    const campoB = document.getElementById('campo-b');
-    console.log(campoB.value)
+$(document).ready(function(){
+    // EVENTOS A ELEMENTOS UTILIZANDO JQUERY
     
 
-    const mensagemSucesso = `O número do campo B (${campoB.value}) é maior que o número do campo A (${campoA.value}), portanto formulário válido.`;
-    const mensagemErro = `O número do campo B (${campoB.value}) é menor ou igual ao número do campo A (${campoA.value}), portanto formulário inválido.`;
-
-    formEstaValido = validaComparacao(Number.parseFloat(campoA.value),Number.parseFloat(campoB.value));
-    console.log(formEstaValido)
-
-    if (!formEstaValido) {
-        const containerMensagemErro = document.querySelector('.mensagem-de-erro');
-        containerMensagemErro.innerHTML = mensagemErro;
-            
-        containerMensagemErro.style.display = 'block';
-
-        campoA.value = '';
-        campoB.value = '';
+    
+    $('#form').on('submit', function(e){
         
-        
-    } else {
-        const containerMensagemSucesso = document.querySelector('.mensagem-de-sucesso');
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
-        
-        containerMensagemSucesso.style.display = 'block';
-        
-        campoA.value = '';
-        campoB.value = '';
+        e.preventDefault();
+        console.log($('#nome-tarefa').val())
+        const nomeNovaTarefa = $('#nome-tarefa').val();
+        const novaTarefa = $(`<li class="nova-tarefa">${nomeNovaTarefa}</li>`);       
+        $(novaTarefa).appendTo('ul');
+        $('#nome-tarefa').val('');
+    });
 
-    }
-    
-
-
-    
-    
+    $('ul').on('click', '.nova-tarefa', function(){
+        $(this).addClass("tarefa-realizada");
+    })
 })
