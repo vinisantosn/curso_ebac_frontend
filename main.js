037d37fade510f9565e90aba7a7fb7797b9f95ad
Vinicius Santos <vinisantoosk@gmail.com>
2023-05-13 18:12:38 -0300

    fix: correção de mensagens

--- main.js
+++ main.js
@@ -1,52 +1,53 @@
 const form = document.getElementById('form');
+const containerMensagemSucesso = document.querySelector('.mensagem-de-sucesso');
+const containerMensagemErro = document.querySelector('.mensagem-de-erro');
 let formEstaValido = false;
+
 
 function validaComparacao(campoUm,campoDois) {
    return campoDois>campoUm;
 }
 
-
-
-
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+form.addEventListener('keyup', function (e){
+    
+    containerMensagemSucesso.classList.replace('on','off')
+    containerMensagemErro.classList.replace('on','off');
     
     const campoA = document.getElementById('campo-a');
     console.log(campoA.value)
     const campoB = document.getElementById('campo-b');
     console.log(campoB.value)
     
-
-    const mensagemSucesso = `O número do campo B (${campoB.value}) é maior que o número do campo A (${campoA.value}), portanto formulário válido.`;
-    const mensagemErro = `O número do campo B (${campoB.value}) é menor ou igual ao número do campo A (${campoA.value}), portanto formulário inválido.`;
+    const mensagemSucesso = `O número do campo B (${campoB.value}) é maior que o número do campo A (${campoA.value}), portanto se enviado o formulário será <strong>válido</strong>.`;
+    const mensagemErro = `O número do campo B (${campoB.value}) é menor ou igual ao número do campo A (${campoA.value}), portanto se enviado o formulário será <strong>inválido</strong>.`;
 
     formEstaValido = validaComparacao(Number.parseFloat(campoA.value),Number.parseFloat(campoB.value));
-    console.log(formEstaValido)
+    // console.log(formEstaValido);
 
-    if (!formEstaValido) {
-        const containerMensagemErro = document.querySelector('.mensagem-de-erro');
+    if (!formEstaValido) {        
         containerMensagemErro.innerHTML = mensagemErro;
-            
-        containerMensagemErro.style.display = 'block';
-
-        campoA.value = '';
-        campoB.value = '';
-        
-        
-    } else {
-        const containerMensagemSucesso = document.querySelector('.mensagem-de-sucesso');
+        containerMensagemErro.classList.replace('off','on');    
+    } else {        
         containerMensagemSucesso.innerHTML = mensagemSucesso;
-        
-        containerMensagemSucesso.style.display = 'block';
-        
-        campoA.value = '';
-        campoB.value = '';
-
+        containerMensagemSucesso.classList.replace('off','on');        
     }
-    
+})
 
 
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
     
+    containerMensagemSucesso.classList.replace('on','off');
+    containerMensagemErro.classList.replace('on','off');
+    
+    const campoA = document.getElementById('campo-a');
+    const campoB = document.getElementById('campo-b');
+
+    formEstaValido = validaComparacao(Number.parseFloat(campoA.value),Number.parseFloat(campoB.value));
+
+    campoA.value = '';
+    campoB.value = '';
+    
+    alert(`Formulário enviado e ${formEstaValido?'válido':'inválido'}.` )
     
 })
